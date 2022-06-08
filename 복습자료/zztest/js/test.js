f06a22b82ceb7cacbc2e3ff0d32a962b8b8522a5
eilab-ljs1898 <ljs1898@eilab.kr>
2022-06-08 18:32:13 +0900

    공부

--- 복습자료/zztest/js/test.js
+++ 복습자료/zztest/js/test.js
@@ -172,7 +172,7 @@ console.log(InternalPointers.privateUrl);  // undefined
 
 
 ////////////////////////
-
+//  https://ko.javascript.info/closure
 
 function makeCounter() {
 	let count = 0;
@@ -193,4 +193,29 @@ function makeCounter() {
 
 
 
+////////////////////////
 
+function inBetween(a, b) {
+	return function(x) {
+		return x >= a && x <= b;
+	};
+}
+
+function inArray(arr) {
+	return function(x) {
+		return arr.includes(x);
+	};
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+console.log( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+
+/////////////////////////////////////////
+
+//  https://ko.javascript.info/new-function
+//  https://ko.javascript.info/arrow-functions-basics
+//  https://ko.javascript.info/intro
