8c49ae5b89267d23efb7c4f76530932acc044654
eilab-ljs1898 <ljs1898@eilab.kr>
2022-06-21 18:08:51 +0900

    .

--- test/js/test.js
+++ test/js/test.js
@@ -3,11 +3,11 @@ let enable = false;
 $(window).on("load", function(){
     if( window.innerWidth > 375 ) {
 		enable = true;
-		console.log(window.innerWidth);
+		console.log(window.innerWidth + "px (화면넓이)");
 		console.log(enable);
 	}
 });
 
 $(window).on("resize", function(){
-    console.log(window.innerWidth);
-})
+    console.log(window.innerWidth + "px (화면넓이)");
+});
