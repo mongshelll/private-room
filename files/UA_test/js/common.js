685cfae048f443088aa35ee50051908645b4e26a
ljs1898 <ljs1898@eilab.kr>
2022-07-12 10:22:44 +0900

    수정

--- files/UA_test/js/common.js
+++ files/UA_test/js/common.js
@@ -269,6 +269,197 @@ function performanceCheck() {
 }
 
 
+//websocket 체크
+function websocketCheck(){
+	var e = (l = performance.timing).connectEnd;
+	var l = l.responseEnd;
+	var a = '<span class="wrltg">Detecting <span>&#9632;</span><span>&#9632;</span><span>&#9632;</span></span>';
+	var ue = "Supported";
+	var re = "Allowed";
+	var Se = "Detection blocked by browser setting(s)/extension(s) or firewall(s).";
+	var t = "Disabled, or blocked by browser setting(s)/extension(s).";
+	var h = "Unknown. Detection blocked by browser setting(s)/extension(s).";
+	var V = "deviceinfo.me";
+
+	try {
+		window.WebSocket || window.MozWebSocket, ID("ws1").innerHTML = a, ID("wsc1").innerHTML = a;
+		var Xt = "";
+
+		function Yt() {
+			zt()
+		}
+
+		function zt() {
+			if (window.WebSocket || window.MozWebSocket) {
+				ID("ws1").innerHTML = "", ID("ws2").innerHTML = ue;
+				try {
+					(WSC = new WebSocket("wss://" + V + ":8000/")).onopen = function() {
+						console.log(WSC)
+						Xt = !0, ID("wsc1").innerHTML = "", ID("wsc2").innerHTML = re, WSC && WSC.close()
+					}, WSC.onerror = function() {
+						!0 !== Xt && (Xt = !0, ID("wsc1").innerHTML = "", ID("wsc2").innerHTML = Se)
+					}
+				} catch (e) {
+					Xt = !0, ID("wsc1").innerHTML = "", ID("wsc2").innerHTML = "Blocked by browser setting(s)/extension(s)."
+				}
+				setTimeout(function() {
+					!0 !== Xt && (Xt = !0, ID("wsc1").innerHTML = "", ID("wsc2").innerHTML = ue)
+				}, 2e3)
+			} else ID("ws1").innerHTML = "", ID("ws2").innerHTML = t, ID("wsc1").innerHTML = "", ID("wsc2").innerHTML = t
+		}
+		window.addEventListener("load", Yt())
+	} catch (e) {
+		ID("ws2").innerHTML = h, ID("wsc2").innerHTML = h
+	}
+}
+
+//webRTC 체크
+function webRTC(){
+	var mn = "",
+		Tn = !1,
+		Ln = "";
+	var t = "Disabled, or blocked by browser setting(s)/extension(s).";
+	var Ie = "Unknown. Blocked by browser setting(s)/extension(s), or not supported.";
+	var u = "Enabled";
+
+	try {
+		var fn = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection,
+			yn, fn;
+		fn || (fn = yn.RTCPeerConnection || yn.mozRTCPeerConnection || yn.webkitRTCPeerConnection), fn ? mn = u : (mn = t, Tn = !0), Ln = !1
+	} catch (e) {
+		mn = Ie, Ln = !0
+	}
+
+	ID("rtc1").innerHTML = mn
+}
+
+
+function webcams(){
+
+	var xe = !1;
+	var Tn = !1;
+	var	Ln = !1;
+	var m = navigator;
+	var T = window;
+	var n = "Unknown. Detection not supported or is blocked by browser setting(s)/extension(s).";
+	var I = "Edge";
+	var y = "";
+	var a = '<div class="wrltg">Detecting <span>&#9632;</span><span>&#9632;</span><span>&#9632;</span></div>';
+	var te = "Detected";
+	var bn = "Unknown. Detection not responding." + ae,
+		vn = "Unknown. WAN IP assigned, or detection may be blocked by browser setting(s)/extension(s) or firewall(s).",
+		Cn = "Unknown. Detection timed out. Detection may be blocked by browser setting(s)/extension(s) or firewall(s).",
+		ae = '<div class="jeeqs"><span>&#9632;</span><span>&#9632;</span><span>&#9632;</span></div>',
+		M = "None detected",
+		g = document,
+		Q = "103.0",
+		d = "";
+
+
+	if (!1 === xe)
+		if (!1 === Tn)
+			if (!1 === Ln) {
+				var La = "",
+					fa = "";
+				try {
+					var ya = new fn({
+							iceServers: []
+						}),
+						Sa = function() {};
+					ya.createDataChannel("")
+				} catch (e) {
+				}
+				!1 !== La ,
+					function() {
+						var t = [],
+							o = [],
+							r = [],
+							s = [],
+							i = (m.mediaDevices && m.mediaDevices.enumerateDevices && (m.enumerateDevices = function(e) {
+								var n = m.mediaDevices.enumerateDevices();
+								n && n.then ? m.mediaDevices.enumerateDevices().then(e).catch(function() {
+									e([])
+								}) : e([])
+							}), !1),
+							l = (("undefined" != typeof MediaStreamTrack && "getSources" in MediaStreamTrack || m.mediaDevices && m.mediaDevices.enumerateDevices) && (i = !0), !1),
+							d = !1,
+							c = !1,
+							p = !1,
+							u = !1;
+						var h = T.fnocc || {},
+							e = (m.getUserMedia || m.mediaDevices && m.mediaDevices.getUserMedia, "Chrome" !== I && "Chrome for iOS" !== I && "Chrome" !== y || 1 != f(Q, ">=", "47") || /^(https:|chrome-extension:)$/g.test(location.protocol || "") || "undefined" != typeof document && "string" == typeof g.domain && g.domain.search && g.domain.search(/localhost|127.0./g), h.load = function(e) {
+								! function(n) {
+									var a;
+									i ? (!m.enumerateDevices && T.MediaStreamTrack && T.MediaStreamTrack.getSources && (m.enumerateDevices = T.MediaStreamTrack.getSources.bind(T.MediaStreamTrack)), !m.enumerateDevices && m.enumerateDevices && (m.enumerateDevices = m.enumerateDevices.bind(m)), m.enumerateDevices ? (t = [], o = [], r = [], s = [], u = p = c = d = l = !1, a = {}, m.enumerateDevices(function(e) {
+										e.forEach(function(e) {
+											var n, i = {};
+											for (n in e) try {
+												"function" != typeof e[n] && (i[n] = e[n])
+											} catch (e) {}
+											a[i.deviceId + i.label + i.kind] || ("audio" === i.kind && (i.kind = "audioinput"), "video" === i.kind && (i.kind = "videoinput"), i.deviceId || (i.deviceId = i.id), i.id || (i.id = i.deviceId), i.label ? ("videoinput" !== i.kind || u || (u = !0), "audioinput" !== i.kind || p || (p = !0)) : (i.CL = !0, "videoinput" === i.kind ? i.label = "Camera " + (s.length + 1) : "audioinput" === i.kind ? i.label = "Microphone " + (o.length + 1) : "audiooutput" === i.kind ? i.label = "Speaker " + (r.length + 1) : i.label = '&#9642; <span class="tkiyy">Unknown. Grant media permissions to detect.</span>', void 0 === h || "Chrome" !== I && "Chrome for iOS" !== I && "Chrome" !== y || 1 != f(Q, ">=", "47") || /^(https:|chrome-extension:)$/g.test(location.protocol || "") || "undefined" != typeof document && "string" == typeof g.domain && g.domain.search && -1 === g.domain.search(/localhost|127.0./g) && (i.label = "Unknown. HTTPS is required for this browser to detect device labels.")), "audioinput" === i.kind && (l = !0, -1 === o.indexOf(i) && o.push(i)), "audiooutput" === i.kind && (d = !0, -1 === r.indexOf(i) && r.push(i)), "videoinput" === i.kind && (c = !0, -1 === s.indexOf(i) && s.push(i)), t.push(i), a[i.deviceId + i.label + i.kind] = i)
+										}), void 0 !== h && (h.czeuj = t, h.dlxjn = l, h.aokkd = d, h.ovtta = c, h.qecpr = u, h.aitwv = p, h.ofgmq = o, h.onnqt = r, h.vbywi = s), n && n()
+										alert(h.aokkd)
+									})) : n && n()) : n && n()
+								}(e = e || function() {})
+							}, h.czeuj = void 0 !== t ? t : [], h.dlxjn = l, h.aokkd = d, h.ovtta = c, h.qecpr = u, h.aitwv = p, h.ofgmq = o, h.onnqt = r, h.vbywi = s, void 0 === h && (T.fnocc = {}), T.RTCPeerConnection || T.mozRTCPeerConnection || T.webkitRTCPeerConnection);
+						h.RTCPeerConnection = void 0 !== e && Object.keys(e.prototype), T.fnocc = h, "undefined" != typeof module && (module.exports = h), "function" == typeof define && define.amd && define("fnocc", [], function() {
+							return h
+						})
+					}();
+				var A = "",
+					ba = [],
+					va = "",
+					Ca = "";
+
+				function Ba(e) {
+					return 1 == e ? te : 0 == e ? M : void 0
+				}
+
+				function Ha(e) {
+					return 1 == e ? "Granted" : 0 == e && !0 === Ca ? "Unknown. HTTPS is required for this browser to get permissions." : 0 == e ? "Not granted, or denied. Permission required to detect correct device labels." : void 0
+				}
+
+				function wa() {
+					alert(Ba(fnocc.aokkd))
+					A = Ba(fnocc.aokkd), fnocc.onnqt.length && (ba = [], va = 1, fnocc.onnqt.forEach(function(e) {
+						ba.push(va++ + ':</div><div class="uaujg"> ' + e.label + "</div>")
+					}), A = '<div class="azylh"><div class="tlgfj">Number of webcams:</div><div class="uaujg"> ' + fnocc.vbywi.length + '</div></div><div class="azylh"><div class="tlgfj">Webcam Permission:</div><div class="uaujg"> ' + Ha(fnocc.qecpr) + '</div></div><div class="azylh"><div class="tlgfj">Label ' + ba.join('<br><div class="tlgfj">Label ')), !0 !== Pa && (ID("qlova").innerHTML = ""), 0 <= A.indexOf("Granted") && (ID("qlova").innerHTML = ""), Ra = !0, A == M ? ID("qlova").innerHTML = A : (ID("qlova").innerHTML = "", ID("frybh").innerHTML = A)
+				}
+
+				function Fa() {
+					fnocc.load(wa)
+				}
+				function ka() {
+					Fa()
+				}
+				"undefined" == typeof fnocc || "Chrome" !== I && "Chrome for iOS" !== I && "Chrome" !== y || 1 != f(Q, ">=", "47") || /^(https:|chrome-extension:)$/g.test(location.protocol || "") ? Ca = !1 : "undefined" != typeof document && "string" == typeof g.domain && g.domain.search && -1 === g.domain.search(/localhost|127.0./g) && (Ca = !0)
+			} else ID("qlova").innerHTML = n;
+		else ID("qlova").innerHTML = ve;
+		else ID("qlova").innerHTML = ke;
+        if (!1 === xe)
+            if (!1 === Tn)
+                if (!1 === Ln) {
+                    var Aa = "",
+                        Na = "",
+                        Pa = "",
+                        xa = "",
+                        Oa = "",
+                        Ra = "";
+					"" === ID("qlova").innerHTML && "" === ID("frybh").innerHTML && (ID("qlova").innerHTML = a),
+					setTimeout(function() {
+						"" === ID("frybh").innerHTML && !0 !== Ra && (ID("qlova").innerHTML = bn)
+                    }, 5e3), setTimeout(function() {
+						"" === ID("frybh").innerHTML && !0 !== Ra && (ID("qlova").innerHTML = Cn)
+                    }, 1e4);
+                    try {
+                        Fa()
+                    } catch (e) {}
+                } else ID("qlova").innerHTML = n;
+        else ID("qlova").innerHTML = ve;
+        else ID("qlova").innerHTML = ke;
+}
+
+
 
 
 
