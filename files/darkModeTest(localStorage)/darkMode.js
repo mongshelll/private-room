6bea6e501cc8272e1ae113107cf9cce77477a62f
ljs1898 <ljs1898@eilab.kr>
2022-12-06 14:53:24 +0900

    로컬스토리지 사용 예제

---
+++ files/darkModeTest(localStorage)/darkMode.js
@@ -0,0 +1,28 @@
+$('.switch').on('click', () => {
+    
+    $('body').toggleClass('dark');
+
+    if($('body').hasClass('dark')){
+        activateDarkMode();
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        deactivateDarkMode();
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
+let mode = localStorage.getItem('darkMode');
+
+if(mode=='enabled'){
+    activateDarkMode();
+} else if(mode == 'disabled'){
+    deactivateDarkMode();
+}
+
+function activateDarkMode(){
+    $('body').addClass('dark');
+}
+
+function deactivateDarkMode(){
+    $('body').removeClass('dark');
+}
