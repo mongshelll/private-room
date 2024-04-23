4bf001eacecf59ef9d820a387d4d7236dccb6353
ljs1898 <ljs1898@eilab.kr>
2024-04-23 15:05:59 +0900

    240423 이미지 추가, 상태 추가

--- project_test/2024-riverbus/pc-web/resource/js/uiux/ui_common.js
+++ project_test/2024-riverbus/pc-web/resource/js/uiux/ui_common.js
@@ -93,42 +93,50 @@ const gpsDataLocal = [
 	{
 		"title": "ship01",
 		"lat": 37.545973,
-		"lng": 126.895501
+		"lng": 126.895501,
+		"state": ""
 	},
 	{
 		"title": "ship02",
 		"lat": 37.539672,
-		"lng": 126.907624 
+		"lng": 126.907624,
+		"state": "state01"
 	},
 	{
 		"title": "ship03",
 		"lat": 37.523469,
-		"lng": 126.943931
+		"lng": 126.943931,
+		"state": ""
 	},
 	{
 		"title": "ship04",
 		"lat": 37.510529,
-		"lng": 126.980407
+		"lng": 126.980407,
+		"state": ""
 	},
 	{
 		"title": "ship05",
 		"lat": 37.527754,
-		"lng": 127.015848
+		"lng": 127.015848,
+		"state": "state01"
 	},
 	{
 		"title": "ship06",
 		"lat": 37.52014,
-		"lng": 127.080187
+		"lng": 127.080187,
+		"state": "state02"
 	},
 	{
 		"title": "ship07",
 		"lat": 37.531256,
-		"lng": 127.059349
+		"lng": 127.059349,
+		"state": "state02"
 	},
 	{
 		"title": "ship08",
 		"lat": 37.523207,
-		"lng": 127.003163
+		"lng": 127.003163,
+		"state": ""
 	}
 ]
 
@@ -157,77 +165,84 @@ const mapMarkerControlFn = (function() {
 	let gpsData;
 
 	return {
-		addMarkers: function(gpsData) {
+		markerControl: function(gpsData) {
 			const mapArea = document.querySelector('[data-target="zoomTarget"]');
 			const imageWidth = mapArea.offsetWidth;
 			const imageHeight = mapArea.offsetHeight;
 
 			gpsData.forEach(data => {
-				const { title, lat, lng } = data;
+				const { title, lat, lng, state } = data;
 				const existingMarker = markers.find(marker => marker.title === title);
 
-				// GPS 좌표가 지도 범위 내에 있는지 확인
-				if (lat >= mapsLatitudeRange[0] && lat <= mapsLatitudeRange[1] && lng >= mapsLongitudeRange[0] && lng <= mapsLongitudeRange[1]) {
-					// 기존 마커가 있는지 확인하고, 있으면 위치만 업데이트
-					if (existingMarker) {
-						const { element, lng: prevLng } = existingMarker;
-						const xPosition = (lng - mapsLongitudeRange[0]) / (mapsLongitudeRange[1] - mapsLongitudeRange[0]) * imageWidth;
-						const yPosition = (mapsLatitudeRange[1] - lat) / (mapsLatitudeRange[1] - mapsLatitudeRange[0]) * imageHeight;
-						const xPositionPercent = (xPosition / imageWidth) * 100;
-						const yPositionPercent = (yPosition / imageHeight) * 100;
-						element.style.left = `${xPositionPercent}%`;
-						element.style.top = `${yPositionPercent}%`;
-						existingMarker.lat = lat;
-						existingMarker.lng = lng;
+				/* GPS 좌표가 지도 범위 내에 있는지 확인 */
+				// if (lat >= mapsLatitudeRange[0] && lat <= mapsLatitudeRange[1] && lng >= mapsLongitudeRange[0] && lng <= mapsLongitudeRange[1]) {
+				// }
 
-						/* 선박 위치에 따른 기울기 변경 */
-						// 경도에 따른 배 이미지 기울기 조절 -45, 45 2가지 방향 클래스, 0 클래스 제거
-						// 각도가 변경되는 기준점 경도좌표를 정하고
-						if ((lng > mapsLongitudeRange[0] && lng < 126.971310) || (lng > 127.026845 && lng < mapsLongitudeRange[1])) {
-							// 45deg
-							element.classList.remove('rotate-minus');
-							element.classList.add('rotate-plus');
-						} else if (lng > 126.971310 && lng < 126.984221) {
-							// 클래스 제거
-							element.classList.remove('rotate-plus');
-							element.classList.remove('rotate-minus');
-						} else {
-							// -45deg
-							element.classList.remove('rotate-plus');
-							element.classList.add('rotate-minus');
-						}
+				/* 기존 마커가 있는지 확인하고, 업데이트(위치, 상태, 진행 방향) */
+				if (existingMarker) {
+					const { element, lng: prevLng } = existingMarker;
+					const xPosition = (lng - mapsLongitudeRange[0]) / (mapsLongitudeRange[1] - mapsLongitudeRange[0]) * imageWidth;
+					const yPosition = (mapsLatitudeRange[1] - lat) / (mapsLatitudeRange[1] - mapsLatitudeRange[0]) * imageHeight;
+					const xPositionPercent = (xPosition / imageWidth) * 100;
+					const yPositionPercent = (yPosition / imageHeight) * 100;
 
-						/* 선박 경도 데이터 변경에 따른 진행방향 확인 */
-						// 경도 비교해서 이미지 클래스 부여
-						const direction = mapMarkerControlFn.checkDirection(prevLng, lng);
-						if (direction === 'right') {
-							// 좌측에서 우측으로 이동하는 경우 클래스 추가
-							element.classList.add('right-direction');
-							element.classList.remove('left-direction');
-						} else if (direction === 'left') {
-							// 우측에서 좌측으로 이동하는 경우 클래스 제거
-							element.classList.add('left-direction');
-							element.classList.remove('right-direction');
-						}
+					/* 선박 위치 업데이트 */
+					element.style.left = `${xPositionPercent}%`;
+					element.style.top = `${yPositionPercent}%`;
+					existingMarker.lat = lat;
+					existingMarker.lng = lng;
+
+					/* 선박 상태 업데이트 */
+					element.dataset.state = state;
+
+					/* 선박 위치에 따른 기울기 업데이트 */
+					// 경도에 따른 배 이미지 기울기 조절 -45, 45 2가지 방향 클래스, 0 클래스 제거
+					// 각도가 변경되는 기준점 경도좌표를 정하고
+					if ((lng > mapsLongitudeRange[0] && lng < 126.971310) || (lng > 127.026845 && lng < mapsLongitudeRange[1])) {
+						// 45deg
+						element.classList.remove('rotate-minus');
+						element.classList.add('rotate-plus');
+					} else if (lng > 126.971310 && lng < 126.984221) {
+						// 클래스 제거
+						element.classList.remove('rotate-plus');
+						element.classList.remove('rotate-minus');
 					} else {
-						// 기존 마커가 없으면 새로 추가
-						const xPosition = (lng - mapsLongitudeRange[0]) / (mapsLongitudeRange[1] - mapsLongitudeRange[0]) * imageWidth;
-						const yPosition = (mapsLatitudeRange[1] - lat) / (mapsLatitudeRange[1] - mapsLatitudeRange[0]) * imageHeight;
-						const xPositionPercent = (xPosition / imageWidth) * 100;
-						const yPositionPercent = (yPosition / imageHeight) * 100;
-						const marker = document.createElement('div');
-						const markerText = document.createElement('span');
-						marker.className = `ships ${title}`;
-						marker.style.left = `${xPositionPercent}%`;
-						marker.style.top = `${yPositionPercent}%`;
-
-						marker.appendChild(markerText);
-						markerText.textContent = title;
-						markerText.className = `ship-name`
-
-						mapArea.appendChild(marker);
-						markers.push({ title, element: marker, lat, lng });
+						// -45deg
+						element.classList.remove('rotate-plus');
+						element.classList.add('rotate-minus');
 					}
+
+					/* 선박 경도 데이터 변경에 따른 진행방향 업데이트 */
+					// 경도 비교해서 이미지 클래스 부여
+					const direction = mapMarkerControlFn.checkDirection(prevLng, lng);
+					if (direction === 'right') {
+						// 좌측에서 우측으로 이동하는 경우 클래스 추가
+						element.classList.add('right-direction');
+						element.classList.remove('left-direction');
+					} else if (direction === 'left') {
+						// 우측에서 좌측으로 이동하는 경우 클래스 제거
+						element.classList.add('left-direction');
+						element.classList.remove('right-direction');
+					}
+				} else {
+					/* 최초 마커 추가 */
+					// 기존 마커가 없으면 새로 추가
+					const xPosition = (lng - mapsLongitudeRange[0]) / (mapsLongitudeRange[1] - mapsLongitudeRange[0]) * imageWidth;
+					const yPosition = (mapsLatitudeRange[1] - lat) / (mapsLatitudeRange[1] - mapsLatitudeRange[0]) * imageHeight;
+					const xPositionPercent = (xPosition / imageWidth) * 100;
+					const yPositionPercent = (yPosition / imageHeight) * 100;
+					const marker = document.createElement('div');
+					const markerText = document.createElement('span');
+					marker.className = `ships ${title}`;
+					marker.style.left = `${xPositionPercent}%`;
+					marker.style.top = `${yPositionPercent}%`;
+
+					marker.appendChild(markerText);
+					markerText.textContent = title;
+					markerText.className = `ship-name`
+
+					mapArea.appendChild(marker);
+					markers.push({ title, element: marker, lat, lng, state });
 				}
 			});
 
@@ -260,7 +275,7 @@ const mapMarkerControlFn = (function() {
 				.then(data => {
 					// 가져온 데이터를 변수에 할당
 					gpsData = data;
-					mapMarkerControlFn.addMarkers(gpsData);
+					mapMarkerControlFn.markerControl(gpsData);
 
 					console.log(gpsData[0].title);
 					console.log(gpsData[0].lng);
@@ -268,12 +283,17 @@ const mapMarkerControlFn = (function() {
 				.catch(error => {
 					console.error('Error fetching GPS data:', error);
 
+					// 테스트 영역
 					console.log('로컬에서 테스트용 데이터 사용 / 업데이트 불가');
 					gpsData = gpsDataLocal;
-					mapMarkerControlFn.addMarkers(gpsData);
+					// 최초 맵 생성
+					mapMarkerControlFn.markerControl(gpsData);
 
 					// 데이터 업데이트 중지
-					clearInterval(gpsUpdate)
+					clearInterval(gpsUpdate);
+					// 업데이트
+					mapMarkerControlFn.markerControl(gpsData);
+					// //테스트 영역
 				});
 		}
 	}
