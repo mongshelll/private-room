1580521078d44cc511a1e10b22c8e3dd307ae878
ljs1898 <ljs1898@eilab.kr>
2024-05-20 11:50:43 +0900

    240520 플러그인 패키지가 아닌 직접 js파일 받아서 연결

--- #of/masonry_plugin_test/my-vite/js/masonry-grid.js
+++ #of/masonry_plugin_test/my-vite/js/masonry-grid.js
@@ -1,34 +1,35 @@
-import Masonry from 'masonry-layout' // npm에서 받은 플러그인 모듈 연결
+// import Masonry from 'masonry-layout' // npm에서 받은 플러그인 모듈 연결
+import './ui_plugin.js'
 
 // 밖으로 내보낼 스크립트 함수에 담아놓기
 export const masonryGrid = () => {
-    const $gridContainer = document.querySelector('.grid');
-    const masonry = new Masonry( $gridContainer, {
-        // options
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        horizontalOrder: true,
-        gutter: 20
-    });
+	const $gridContainer = document.querySelector('.grid');
+	const masonry = new Masonry( $gridContainer, {
+		// options
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer',
+		horizontalOrder: true,
+		gutter: 20
+	});
 
 
 
-    /* filter 기능 */
-    const filterGrid = triggerBtn => {
-        const gridItems = $gridContainer.querySelectorAll("[data-category]");
-        const targetCategory = triggerBtn.dataset.target;
+	/* filter 기능 */
+	const filterGrid = triggerBtn => {
+		const gridItems = $gridContainer.querySelectorAll("[data-category]");
+		const targetCategory = triggerBtn.dataset.target;
 
-        if (targetCategory) {
-            gridItems.forEach(item => {
-                const shouldShow = targetCategory === "all" || item.dataset.category === targetCategory;
-                item.classList.toggle('is-hidden', !shouldShow);
-            });
-            masonry.layout();
-        }
-    };
+		if (targetCategory) {
+			gridItems.forEach(item => {
+				const shouldShow = targetCategory === "all" || item.dataset.category === targetCategory;
+				item.classList.toggle('is-hidden', !shouldShow);
+			});
+			masonry.layout();
+		}
+	};
 
-    const $filterBtnWrap = document.querySelector('.btn-wrap');
-    $filterBtnWrap.addEventListener("click", (e) => {
-        filterGrid(e.target);
-    })
+	const $filterBtnWrap = document.querySelector('.btn-wrap');
+	$filterBtnWrap.addEventListener("click", (e) => {
+		filterGrid(e.target);
+	})
 }
--- #of/masonry_plugin_test/my-vite/main.js
+++ #of/masonry_plugin_test/my-vite/main.js
@@ -7,5 +7,9 @@
 // import './css/ui_style.css' // 화면에서 사용할 공통 css파일 연결, 빌드된 화면 소스에서 style 태그 내부에 인라인으로 css내용 전부 출력
 import { masonry_component } from './component/masonry_component.js' // 사용할 컴포넌트 가져와서 연결
 
-document.querySelector('#app').innerHTML = masonry_component // 화면에 컴포넌트 그려주기
+document.querySelector('#app').innerHTML = `
+<main>
+  ${masonry_component} <!-- 화면에 컴포넌트 그려주기 -->
+</main>
+`
 
