eea3eafbb3fe2604b2556f40c3ac5e7076d925f2
ljs1898 <ljs1898@eilab.kr>
2024-05-21 10:03:47 +0900

    240521 masonry 수정

--- _of/masonry_plugin_test/my-vite/js/masonry-grid.js
+++ _of/masonry_plugin_test/my-vite/js/masonry-grid.js
@@ -4,23 +4,32 @@ import Masonry from 'masonry-layout' // npm에서 받은 플러그인 모듈 연
 // 밖으로 내보낼 스크립트 함수에 담아놓기
 export const masonryGrid = () => {
 	const $gridContainer = document.querySelector('.grid');
+	const $gridItems = $gridContainer.querySelectorAll("[data-category]");
+	const setDuration = "400ms"; // 기본 duration 정의
 	const masonry = new Masonry( $gridContainer, {
 		// options
 		itemSelector: '.grid-item',
 		columnWidth: '.grid-sizer',
 		horizontalOrder: true,
-		gutter: 20
+		gutter: 20,
+		transitionDuration: setDuration,
+		stagger: 5
 	});
 
-
+	// .layout() 실행전 duration이 없기 때문에 초기화 실행, css로 값을 주면 이동 후 떨림 현상이 있음
+	masonry.once('layoutComplete', () => {
+		$gridItems.forEach(item => {
+			item.style.transitionDuration = setDuration
+		})
+		console.log('start');
+	})
+	masonry.layout();
 
 	/* filter 기능 */
 	const filterGrid = triggerBtn => {
-		const gridItems = $gridContainer.querySelectorAll("[data-category]");
 		const targetCategory = triggerBtn.dataset.target;
-
 		if (targetCategory) {
-			gridItems.forEach(item => {
+			$gridItems.forEach(item => {
 				const shouldShow = targetCategory === "all" || item.dataset.category === targetCategory;
 				item.classList.toggle('is-hidden', !shouldShow);
 			});
